d3737af800653540c8d05077a96f98cd6a0a20cb
lcc-1314 <1535504121@qq.com>
2019-12-03 17:24:37 +0800

    上传src

---
+++ src/HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
+/*
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("123435");
+    }
+}
+*/
